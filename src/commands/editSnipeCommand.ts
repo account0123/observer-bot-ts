1760679104cf21b275524827ce38b11794f2065d
NewCreative <ianparra200@gmail.com>
2020-08-31 15:31:54 -0400

    snipe command embed has bot color

--- src/commands/editSnipeCommand.ts
+++ src/commands/editSnipeCommand.ts
@@ -8,6 +8,8 @@ export class EditSnipeCommand implements Command {
 	shortdescription: string = 'Muestra el mensaje editado.'
 	fulldescription: string = 'Muestra el último mensaje editado de este canal'
 	async run(msg: Message): Promise<void> {
+		const bot = msg.guild!.member(msg.client.user!)!
+		const color = bot.displayColor
 		const connection = Connections.connection
 		connection.query('SELECT * FROM edited WHERE ? and ?',[msg.channel.id, msg.guild!.id],function (err:any, rows) {
 			if(err) throw err
@@ -16,7 +18,7 @@ export class EditSnipeCommand implements Command {
 				return
 			}
 			const lastrow = rows[rows.length - 1]
-			const embed = new MessageEmbed().setAuthor(lastrow.username,lastrow.avatar_url).setDescription(lastrow.content).setFooter('Mensaje editado').setTimestamp(lastrow.time);
+			const embed = new MessageEmbed().setAuthor(lastrow.username,lastrow.avatar_url).setColor(color).setDescription(lastrow.content).setFooter('Mensaje editado').setTimestamp(lastrow.time);
 			msg.channel.send(embed).catch(e=>{
 				msg.reply('Hubo un error al enviar el editsnipe')
 				console.error(e.stack)
--- src/commands/snipeCommand.ts
+++ src/commands/snipeCommand.ts
@@ -9,6 +9,8 @@ export class SnipeCommand implements Command {
 	shortdescription: string = 'Muestra el mensaje borrado.'
 	fulldescription: string = 'Muestra el último mensaje borrado de este canal'
 	async run(msg: Message): Promise<void> {
+		const bot = msg.guild!.member(msg.client.user!)!
+		const color = bot.displayColor
 		const connection = Connections.connection
 		connection.query('SELECT * FROM deleted WHERE ? and ?',[msg.channel.id, msg.guild!.id],function (err:any, rows) {
 			if(err) throw err
@@ -17,7 +19,7 @@ export class SnipeCommand implements Command {
 				return
 			}
 			const lastrow = rows[rows.length - 1]
-			const embed = new MessageEmbed().setAuthor(lastrow.username,lastrow.avatar_url).setDescription(lastrow.content).setFooter('Mensaje borrado').setTimestamp(lastrow.time);
+			const embed = new MessageEmbed().setAuthor(lastrow.username,lastrow.avatar_url).setColor(color).setDescription(lastrow.content).setFooter('Mensaje borrado').setTimestamp(lastrow.time);
 			msg.channel.send(embed).catch(e=>{
 				msg.reply('Hubo un error al enviar el snipe')
 				console.error(e.stack)
