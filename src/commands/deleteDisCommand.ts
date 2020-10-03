031eb469dd3c11c25832d0b3a6e97a6c6220b49d
NewCreative <ianparra200@gmail.com>
2020-10-03 01:10:11 -0300

    v1.0.5a

---
+++ src/commands/deleteDisCommand.ts
@@ -0,0 +1,15 @@
+import { Message } from "discord.js";
+import Command from "./commandInterface";
+import { Lang } from "./lang/Lang";
+
+export class DeleteDisCommand implements Command {
+	commandNames: string[] = ['deletedis', 'deletethis', 'supr']
+	guildExclusive: boolean = false
+	shortdescription: string = 'info.deletedis.description'
+	fulldescription: string = this.shortdescription
+	permission: string = ''
+	async run(msg: Message, l: Lang): Promise<void> {
+		await msg.channel.bulkDelete(2)
+	}
+	
+}
