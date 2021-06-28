ce45db733563153c36e41209389a62d3ff6d2abc
NewCreative <ianparra200@gmail.com>
2021-06-28 17:08:16 -0400

    bug fixed
    really, it's easier to delete them from commandHandler.ts

--- src/commandHandler.ts
+++ src/commandHandler.ts
@@ -1,5 +1,5 @@
 import { Message } from "discord.js";
-import {StopCommand, AvatarCommand, CreateRoleCommand, BanCommand, SayCommand, AddRoleCommand, EditRoleCommand, CleanCommand, DemoteCommand, RemoveRoleCommand, HelpCommand, GetPassCommand, KickCommand, RoleInfoCommand, ServerInfoCommand, ResetAllRolesCommand, UserInfoCommand, SnipeCommand, EditSnipeCommand, UnbanCommand, CallCommand, LangCommand, InfoCommand, FocusBanCommand, FormatCommand, CodeCommand, CancelCommand, FocusKickCommand, CreateChannelCommand, DeleteDisCommand, ResetMemberCommand, RenameEveryoneCommand, MuteCommand, WhisperCommand, SetCommand, RAECommand, EditChannelCommand, WebhooksCommand, CreateWebhookCommand, CopyCommand } from "./commands";
+import {StopCommand, AvatarCommand, CreateRoleCommand, BanCommand, SayCommand, AddRoleCommand, EditRoleCommand, CleanCommand, DemoteCommand, RemoveRoleCommand, HelpCommand, GetPassCommand, KickCommand, RoleInfoCommand, ServerInfoCommand, ResetAllRolesCommand, UserInfoCommand, SnipeCommand, EditSnipeCommand, UnbanCommand, LangCommand, InfoCommand, FocusBanCommand, FormatCommand, CodeCommand, CancelCommand, FocusKickCommand, CreateChannelCommand, DeleteDisCommand, ResetMemberCommand, RenameEveryoneCommand, SetCommand, RAECommand, EditChannelCommand, WebhooksCommand, CreateWebhookCommand, CopyCommand } from "./commands";
 import Command from "./commands/commandInterface";
 import { CommandParser } from "./models/commandParser";
 import ArgCommand from "./commands/commandArgInterface";
@@ -41,7 +41,6 @@ export default class CommandHandler {
       RoleInfoCommand,
       ResetAllRolesCommand,
       UserInfoCommand,
-      CallCommand,
       LangCommand,
       FocusBanCommand,
       FormatCommand,
@@ -51,8 +50,6 @@ export default class CommandHandler {
       CreateChannelCommand,
       ResetMemberCommand,
       RenameEveryoneCommand,
-      MuteCommand,
-      WhisperCommand,
       SetCommand,
       RAECommand,
       EditChannelCommand,
