a8b3ca4ce42691c865fdcc1cbe2f619cf34f268d
account0123 <ianparra200@gmail.com>
2021-08-31 17:38:54 -0400

    Permissions for use bot added
    When a role is updated or created adding administrator permission, it grants total access to bot moderation commands.
    
    When a role is deleted and it had access to bot commands, the role is deleted from database
    
    When a guild is deleted, all the configs, users and roles are deleted from database

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import express, { Request, Response } from 'express';
-import Discord, { Message, PartialMessage } from "discord.js";
+import Discord, { Message, PartialMessage, Role } from "discord.js";
 import { DISCORD_TOKEN } from './config/secrets';
 import CommandHandler from './commandHandler';
 import SnipeHandler from './snipeHandler';
 import { Connections } from './config/connections';
+import { RowDataPacket } from 'mysql2';
 
 const PORT = parseInt(process.argv[2]) || 5000;
 
@@ -36,16 +37,52 @@ client.on("ready", async () => {
     const q = Connections.db.query('INSERT INTO guilds VALUES (?, ?, ?, ?, ?) ON DUPLICATE KEY UPDATE id=id;', [g.id, g.name, '!!', 'es', null]);
     q.then(()=>console.log('Servidor cargado: ' + g.id));
     q.catch(e=>console.error(e));
+    const [qr] = await Connections.db.query<RowDataPacket[]>('SELECT id FROM roles WHERE guild=?', [g.id])
+    const roles = qr.map(row=>row.id)
+    for (const role of g.roles.cache.values()) {
+      if(roles.includes(role.id)) continue;
+      if(role.permissions.has('ADMINISTRATOR')) addAdminRole(role)
+    }
   }
 });
 client.on("message", (message: Message)=>handler.handleMessage(message));
 client.on('messageDelete',(deleted: Message| PartialMessage)=>sniper.saveDeletedMessage(deleted))
 client.on('messageUpdate',(old: Message | PartialMessage, updated: Message | PartialMessage)=>sniper.saveEditedMessage(old, updated))
-client.on('guildCreate', guild => {
+client.on('guildCreate', async guild => {
   const q = Connections.db.query('INSERT INTO guilds VALUES (?, ?, ?, ?, ?) ON DUPLICATE KEY UPDATE id=id;', [guild.id, guild.name, '!!', 'es', null]);
   q.then(()=>console.log('Servidor nuevo registrado: ' + guild.name));
   q.catch(e=>console.error(e));
+  const [qr] = await Connections.db.query<RowDataPacket[]>('SELECT id FROM roles WHERE guild=?', [guild.id])
+  const roles = qr.map(row=>row.id)
+  for (const role of guild.roles.cache.values()) {
+    if(roles.includes(role.id)) continue;
+    if(role.permissions.has('ADMINISTRATOR')) addAdminRole(role)
+  }
 });
+client.on("guildDelete", guild=>{
+  const q1 = Connections.db.query('DELETE FROM guilds WHERE id=?', [guild.id])
+  q1.then(()=>console.log('Servidor ' + guild.name + ' eliminado de la database por ser eliminado de discord'));
+  q1.catch(e=>console.error(e));
+  const q2 = Connections.db.query("DELETE FROM users WHERE guild=?", [guild.id])
+  q2.then(()=>console.log('Registros de los miembros de' + guild.name + ' eliminados de la database por servidor eliminado de discord'));
+  q2.catch(e=>console.error(e));
+  const q3 = Connections.db.query("DELETE FROM roles WHERE guild=?", [guild.id])
+  q3.then(()=>console.log('Registros de roles' + guild.name + ' eliminados de la database por servidor eliminado de discord'));
+  q3.catch(e=>console.error(e));
+})
+client.on("roleCreate", (role: Role)=>{
+  if(!role.permissions.has('ADMINISTRATOR')) return
+  addAdminRole(role)
+})
+client.on("roleDelete", (role: Role)=>{
+  const q = Connections.db.query("DELETE FROM roles WHERE id=?", [role.id])
+  q.then(()=>console.log(`Rol ${role.name} (${role.id}), que fue eliminado de su servidor, eliminado de la database`))
+  q.catch(e=>console.error(e))
+})
+client.on("roleUpdate", (old_role: Role, new_role: Role) => {
+  if(old_role.permissions.missing('ADMINISTRATOR') && new_role.permissions.has('ADMINISTRATOR'))
+    addAdminRole(new_role);
+})
 
 client.on("error", e =>{
   console.error("Discord client error!", e)
@@ -57,3 +94,9 @@ if(process.argv[2] == '--beta') client.login('NzA4ODg0MjYwNjY0MjQ2MzI0.Xrd16g.3C
 else client.login(DISCORD_TOKEN).catch(e=>console.error(e));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}!`))
+
+function addAdminRole(role: Role){
+  const q =Connections.db.query("INSERT INTO roles VALUES (?, ?, ?, ?) ON DUPLICATE KEY UPDATE id=id", [role.id, role.guild.id, 'adm', 0b01111111])
+  q.then(()=>console.log(`Rol ${role.name} (${role.id}) de ${role.guild.name} agregado como rol de administrador`))
+  q.catch(e=>console.error(e))
+}
